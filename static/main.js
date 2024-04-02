bc56a1bcf6185e5a8f46a0608e24fab4491b06b9
daniel-rogerio <danielr.xp@gmail.com>
2024-04-01 21:07:24 -0300

    feat: contrução do controle dos alongamentos.
    Construído regra para adicionar mais alongamentos para não repetir.
    Construído regra para salvar os alongamentos em um 'array'.
    Construído regra para mostrar novos exercícios.
    Comentários no código JavaScript.
    Ajustes gerais.

--- static/main.js
+++ static/main.js
@@ -1,32 +1,44 @@
+// Botões de controle do Pomodoro Timer:
 const startBtn = document.getElementById('start');
 const resetBtn = document.getElementById('reset');
 const stopBtn = document.getElementById('stop');
 
+// Tempo do Pomodoro Timer:
 const workMinutes = document.getElementById('work-minutes');
 const workSeconds = document.getElementById('work-seconds');
 
+// Contador de ciclos realizados:
 const cycles = document.getElementById('cycles');
 
+// Tempo do descanso:
 const breakMinutes = document.getElementById('break-minutes');
 const breakSeconds = document.getElementById('break-seconds');
 
+// Controle de exibição do cartão de alongamentos:
 const showStretches = document.getElementById('show-stretches');
+
+// Botões de controle do cartão de alongamentos:
 const continueStretchesBtn = document.getElementById('stretches-start');
 const stopStretchesBtn = document.getElementById('stretches-pause');
 const finishStretchesBtn = document.getElementById('stretches-finish');
 
+// Variaveis de controle:
 var startTimer;
+let stretchesList = [];
+let currentStretche = 0;
+let offset = 0;
 
-showStretches.style.display = 'none';
-
+// Evento para iniciar o Pomodoro Timer:
 startBtn.addEventListener('click', () => {
   if(startTimer === undefined) {
+    getStretches();
     startTimer = setInterval(timer, 1000);
   } else {
-    alert('O timer já está executando!');
+    alert('The timer is already running!');
   }
 });
 
+// Evento para resetar o Pomodoro Timer:
 resetBtn.addEventListener('click', () => {
   workMinutes.innerText = 25;
   workSeconds.innerText = '00';
@@ -39,45 +51,48 @@ resetBtn.addEventListener('click', () => {
   startTimer = undefined;
 });
 
+// Evento para parar o Pomodoro Timer:
 stopBtn.addEventListener('click', () => {
   if (startTimer !== undefined) {
     stopTimer();
     startTimer = undefined;
   } else {
-    alert('O timer já está pausado!');
+    alert('The timer is already paused!');
   }
 });
 
+// Evento para iniciar o Pomodoro Timer usando os botões de controle dos alongamentos:
 continueStretchesBtn.addEventListener('click', () => {
   if(startTimer === undefined) {
     startTimer = setInterval(timer, 1000);
   } else {
-    alert('O timer já está executando!');
+    alert('The timer is already running!');
   }
 });
 
+// Evento para parar o Pomodoro Timer usando os botões de controle dos alongamentos:
 stopStretchesBtn.addEventListener('click', () => {
   if (startTimer !== undefined) {
     stopTimer();
     startTimer = undefined;
   } else {
-    alert('O timer já está pausado!');
+    alert('The timer is already paused!');
   }
 });
 
+// Evento para concluir o alongamento e iniciar um novo ciclo:
 finishStretchesBtn.addEventListener('click', () => {
   recycle();
 
   if(startTimer === undefined) {
     startTimer = setInterval(timer, 1000);
-  } else {
-    alert('O timer já está executando!');
   }
 
   showStretches.style.display = 'none';
 
 });
 
+// Função para controlar o Pomodoro Timer:
 function timer() {
   if (workSeconds.innerText != 0) {
     workSeconds.innerText--;
@@ -87,6 +102,7 @@ function timer() {
   }
 
   if (workMinutes.innerText == 0 && workSeconds.innerText == 0) {
+    setStretches();
     showStretches.style.display = 'flex';
     if (breakSeconds.innerText != 0) {
       breakSeconds.innerText--;
@@ -102,29 +118,56 @@ function timer() {
   }
 }
 
+// Função para resetar o Pomodoro Timer e iniciar um novo ciclo:
 function recycle() {
   workMinutes.innerText = 25;
   workSeconds.innerText = '00';
   breakMinutes.innerText = 5;
   breakSeconds.innerText = '00';
   cycles.innerText++;
+
+  if (currentStretche === 9) {
+    offset += 10;
+    currentStretche = 0;
+    getStretches()
+    return
+  } else {
+    currentStretche++;
+  }
 }
 
+// Função para parar o Pomodoro Timer:
 function stopTimer() {
   clearInterval(startTimer);
 }
 
+// Função para buscar os dados da API dos alongamentos via 'fetch':
 function getStretches() {
-  fetch('https://api.api-ninjas.com/v1/exercises?type=stretching', {
+  fetch('https://api.api-ninjas.com/v1/exercises?type=stretching&offset=' + offset, {
     method: 'GET',
-    headers: { 'X-Api-Key': 'MY-API-KEY' },
+    headers: { 'X-Api-Key': 'MY_API_KEY' },
     contentType: 'application/json',
   })
   .then(response => response.json())
   .then(data => {
+    stretchesList = data;
     console.log(data);
   })
   .catch(error => console.log(error));
 }
 
-getStretches();
+// Função para atualizar o cartão dos alongamentos com as informações dos alongamentos:
+function setStretches() {
+  const exerciceName = document.getElementById('exercice-name');
+  const exerciceType = document.getElementById('exercice-type');
+  const exerciceMuscle = document.getElementById('exercice-muscle');
+  const exerciceDifficult = document.getElementById('exercice-difficult');
+  const exerciceInstruction = document.getElementById('exercice-instruction');
+
+  exerciceName.innerText = stretchesList[currentStretche].name;
+  exerciceType.innerText = `Type: ${stretchesList[currentStretche].type}`;
+  exerciceMuscle.innerText = `Muscle: ${stretchesList[currentStretche].muscle}`;
+  exerciceDifficult.innerText = `Difficult: ${stretchesList[currentStretche].difficulty}`;
+  exerciceInstruction.innerText = `INSTRUCTIONS:\n\n${stretchesList[currentStretche].instructions}`;
+
+}
